dbe288e8769dc2116c80b326fa81ebe6a99f498d
Marc <webdev@marcbakker.com>
2025-12-31 17:53:22 +0100

    Stop met het importeren van firebase.config.ts en maak een environment.ts aan die de variabelen uitleest.

--- apps/sollicitaties/src/app/app.config.ts
+++ apps/sollicitaties/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
-import { firebaseConfig } from './environments';
+import { environment } from './environments';
 import { routes } from './app.routes';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 
@@ -11,7 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth())
   ]
