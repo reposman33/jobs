6980762af0b06936f4391d46e0b00c735544c019
Marc <webdev@marcbakker.com>
2026-01-02 16:16:35 +0100

    verander de process.env. keys van UPPER_CASE naar camelCase

--- set-env.js
+++ set-env.js
@@ -5,12 +5,12 @@ const path = require('path');
 const targetPath = path.join(__dirname, 'apps/sollicitaties/src/app/environments/firebase.config.ts');
 
 const envConfigFile = `export const firebaseConfig = {
-  apiKey: '${process.env.API_KEY || ""}',
-  authDomain: '${process.env.AUTH_DOMAIN || ""}',
-  projectId: '${process.env.PROJECT_ID || ""}',
-  storageBucket: '${process.env.STORAGE_BUCKET || ""}',
-  messagingSenderId: '${process.env.MESSAGING_SENDER_ID || ""}',
-  appId: '${process.env.APP_ID || ""}'
+  apiKey: '${process.env.apiKey || ""}',
+  authDomain: '${process.env.authDomain || ""}',
+  projectId: '${process.env.projectId || ""}',
+  storageBucket: '${process.env.storageBucket || ""}',
+  messagingSenderId: '${process.env.messagingSenderId || ""}',
+  appId: '${process.env.appId || ""}'
 };
 `;
 
