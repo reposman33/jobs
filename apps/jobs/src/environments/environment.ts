caa60e0bcf2ca9dae39d0ae5eb5c34a6ba8ad274
Marc Bakker <3200870+reposman33@users.noreply.github.com>
2026-03-02 19:55:40 +0100

    Update environment.ts
    er waren per ongeluk secrets mee gecommit

--- apps/jobs/src/environments/environment.ts
+++ apps/jobs/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyASBP5jD_9ddk1Gq0X1xUZizl-eRZUgDWI",
-    authDomain: "jobs-marcb.firebaseapp.com",
-    projectId: "jobs-marcb",
-    storageBucket: "jobs-marcb.firebasestorage.app",
-    messagingSenderId: "18104146140",
-    appId: "1:18104146140:web:f3976c7176078028213c19",
-    measurementId: "G-KHD5H0W7J0"  
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""  
   },
   production: false,
   currentBuildCommitSha: null,
