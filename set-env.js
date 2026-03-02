31f6a9238a7d82585d7169f9892cefe3d0d8eb33
Marc <webdev@marcbakker.com>
2026-03-02 13:43:12 +0100

    De environments folder moet hier staan: apps/sollicitaties/src/environments/ ❌ Niet in: apps/sollicitaties/src/app/environments/

--- set-env.js
+++ set-env.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 const path = require('path');
 
 // Pad vanaf de root naar je environments map
-const targetPath = path.join(__dirname, 'apps/sollicitaties/src/app/environments/firebase.config.ts');
+const targetPath = path.join(__dirname, 'apps/sollicitaties/src/environments/firebase.config.ts');
 
 const envConfigFile = `export const firebaseConfig = {
-  apiKey: '${process.env.apiKey || ""}',
+  apiKey: '${process.env.apiKey || ""},
   authDomain: '${process.env.authDomain || ""}',
   projectId: '${process.env.projectId || ""}',
   storageBucket: '${process.env.storageBucket || ""}',
