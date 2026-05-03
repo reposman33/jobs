0fa6eed6b007376c12bea6c244abf989668aa5e1
Marc <webdev@marcbakker.com>
2026-05-04 00:37:12 +0200

    Implement search functionality in Search component with reactive form and event emitter. Update StorageService to filter jobs based on search text. Modify jobs component template to integrate search component.

--- apps/jobs/src/app/components/search/search.ts
+++ apps/jobs/src/app/components/search/search.ts
@@ -1,13 +1,40 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './search.html',
   styleUrl: './search.scss',
   encapsulation: ViewEncapsulation.Emulated,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Search {
+  @Output() search = new EventEmitter<string>();
 
+  searchForm = new FormGroup({
+      search: new FormControl('')
+  });
+
+  ngOnInit() {
+    this.searchForm.get('search')?.valueChanges.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    );
+  }
+
+  onSearch() {
+    const query = this.searchForm.get('search')?.value;
+    this.search.emit(query ?? '');
+  }
 }
