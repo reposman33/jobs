54249605ce83c2ae74e0cde817a3d2a9a1b8e8e3
Marc <webdev@marcbakker.com>
2026-05-05 13:16:13 +0200

    Revert "Voegtoe vercel-speedInsights"
    This reverts commit 35154951f571d3b024da24fadcc296445343a992.

--- apps/jobs/src/app/app.ts
+++ apps/jobs/src/app/app.ts
@@ -1,6 +1,5 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SpeedInsights } from '@vercel/speed-insights';
 import { Header } from './components/header/header';
 
 @Component({
@@ -12,12 +11,4 @@ import { Header } from './components/header/header';
 })
 export class App {
   protected readonly title = signal('sollicitatie');
-  speedInsights: SpeedInsights = inject(SpeedInsights)
-  
-  ngOnInit() {
-    this.speedInsights();
-  }
-  // constructor(private speedInsights: SpeedInsights) {
-  //   this.speedInsights();
-  // } 
 }
