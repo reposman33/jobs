35154951f571d3b024da24fadcc296445343a992
Marc <webdev@marcbakker.com>
2026-05-05 13:02:08 +0200

    Voegtoe vercel-speedInsights

--- apps/jobs/src/app/app.ts
+++ apps/jobs/src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SpeedInsights } from '@vercel/speed-insights';
 import { Header } from './components/header/header';
 
 @Component({
@@ -11,4 +12,12 @@ import { Header } from './components/header/header';
 })
 export class App {
   protected readonly title = signal('sollicitatie');
+  speedInsights: SpeedInsights = inject(SpeedInsights)
+  
+  ngOnInit() {
+    this.speedInsights();
+  }
+  // constructor(private speedInsights: SpeedInsights) {
+  //   this.speedInsights();
+  // } 
 }
