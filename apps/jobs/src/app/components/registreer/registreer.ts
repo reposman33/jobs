8951d9d1ea9124d2fcffc05c1137e80acdf2f48b
Marc <webdev@marcbakker.com>
2026-05-19 15:13:00 +0200

    verwijder Angular/Material component, Tailwindcss classes.

--- apps/jobs/src/app/components/registreer/registreer.ts
+++ apps/jobs/src/app/components/registreer/registreer.ts
@@ -4,14 +4,13 @@ import {
   inject,
   ViewEncapsulation,
 } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth-service';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [MatButtonModule, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './registreer.html',
   styleUrl: './registreer.scss',
   encapsulation: ViewEncapsulation.Emulated,
