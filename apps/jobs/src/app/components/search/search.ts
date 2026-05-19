37840e5e04bdb14e823d0b2115955d99d55ef9e0
Marc <webdev@marcbakker.com>
2026-05-19 15:14:16 +0200

    verwijder Angular/Material component, Tailwindcss classes.

--- apps/jobs/src/app/components/search/search.ts
+++ apps/jobs/src/app/components/search/search.ts
@@ -1,17 +1,9 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-search',
   imports: [
     ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatIconModule
   ],
   templateUrl: './search.html',
   styleUrl: './search.scss',
